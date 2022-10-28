232650b59f6ff414110a202f40adf77ec9219106
DC1823 <cue21965@uvg.edu.gt>
2022-10-27 20:19:47 -0600

    Login

---
+++ Docerify/Docerify/app/src/main/java/com/example/shashank/login/DashboardActivity.java
@@ -0,0 +1,37 @@
+package com.example.shashank.login;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    String EmailHolder;
+    TextView Email;
+    Button LogOUT ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        Email = (TextView)findViewById(R.id.textView1);
+        LogOUT = (Button)findViewById(R.id.button1);
+
+        Intent intent = getIntent();
+        EmailHolder = intent.getStringExtra(MainActivity.UserEmail);
+        Email.setText(Email.getText().toString()+ EmailHolder);
+        LogOUT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                Toast.makeText(DashboardActivity.this,"Log Out Successful", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
